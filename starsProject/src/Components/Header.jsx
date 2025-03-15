b296e3e500dff233b192bbe0ae5cbfdcd550c5bc
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-14 22:13:27 -0700

    working with the header div box

--- starsProject/src/Components/Header.jsx
+++ starsProject/src/Components/Header.jsx
@@ -4,10 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="headerContainer">
-      <h1 className="header">Traversing The Stars</h1>
-      <h4 className="subTitle">It's easier than you might Think!</h4>
+      <div className="headerText">
+        <h1 className="header">Traversing The Stars</h1>
+        <h4 className="subTitle">It's easier than you might Think!</h4>
+      </div>
     </div>
-
   );
 };
 
