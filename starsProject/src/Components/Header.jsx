85b44ad9b2111d2fea494988f1825beea65ee8ac
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-14 22:21:59 -0700

    a solid header start

--- starsProject/src/Components/Header.jsx
+++ starsProject/src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="headerContainer">
       <div className="headerText">
-        <h1 className="header">Traversing The Stars</h1>
+        <h1 className="header">Traversing the Stars</h1>
         <h4 className="subTitle">It's easier than you might Think!</h4>
       </div>
     </div>
