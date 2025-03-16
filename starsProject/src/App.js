bccc64bcc97655b8baad9e66a92326da03733deb
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-16 12:29:36 -0700

    updated button to card

--- starsProject/src/App.js
+++ starsProject/src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Landing from "./Components/Landing/Landing";
-import Button from "./Components/Button/Button";
+import Card from "./Components/Card/Card";
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [star, getStar] = useState([]);
+
+  useEffect(() => {
+    fetchStars();
+  }, []);
+
+  const fetchStars = async () => {
+    let res = await fetch("http://localhost:3000/posts");
+    //always convert to json
+    let data = await res.json();
+
+    console.log(data);
+  };
   return (
     <div className="body">
       <Header />
       <Landing />
-      <Button/>
+      <Card/>
     </div>
   );
 }
