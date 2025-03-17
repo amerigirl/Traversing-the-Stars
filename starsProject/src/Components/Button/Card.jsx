a1036fb5cfa16f6daec624dd2042331e70d86958
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-16 19:30:12 -0700

    working on the css for card

--- starsProject/src/Components/Button/Card.jsx
+++ starsProject/src/Components/Button/Card.jsx
@@ -3,13 +3,26 @@ import "./Card.css";
 
 const Card = ({ star }) => {
   return (
-    <div className="card-container">
-      {star.map((s, index) => {
-        return <button key={index} className="card-item">
-          {s.name}
-        </button>
-      })}
-    </div>
+    <>
+      <div className="container">
+        <div className="card-container">
+          {star.map((s, index) => {
+            return (
+              <button key={index} className="card-item">
+                {s.name}
+              </button>
+            );
+          })}
+        </div>
+        <div className="img-container">
+          <img
+            className="img"
+            alt="watchingTheStars"
+            src="./LookingAtConstellations.jpeg"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
