7d9cf6e8ab24e04c90499036468ce3a65e92a869
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-17 22:12:48 -0700

    updated CONFUSING prop names

--- starsProject/src/Components/Card/CardDetails/CardDetails.jsx
+++ starsProject/src/Components/Card/CardDetails/CardDetails.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 const CardDetails = ({ selectedStar }) => {
-  if (!selectedStar) {
-    return <div>Select a star to view details</div>;
-  }
+  // if (!selectedStar) {
+  //   return <div>Select a star to view details</div>;
+  // }
 
   return (
-    <div>
-      <h2>{selectedStar.name}</h2>
-      <p>Constellation: {selectedStar.constellation}</p>
-      <p>Distance: {selectedStar.distance} light years</p>
-    </div>
+    <>
+      <div>
+        <h2>{selectedStar.name}</h2>
+        <p>Also known as: {selectedStar.alsoKnownAs}</p>
+        <p>Distance: {selectedStar.description}</p>
+      </div>
+      <div>
+        <img src={selectedStar.image} />
+      </div>
+    </>
   );
 };
 
